b064b1f02ed5b776aa8a643e4fe3504c4861c050
Grzegorz Sowa <grzegorz.sowa314@gmail.com>
2022-02-22 22:48:01 +0100

    remove override from TenantContext

--- src/main/java/pl/recompiled/springtenantseparationdemo/security/user/TenantContext.java
+++ src/main/java/pl/recompiled/springtenantseparationdemo/security/user/TenantContext.java
@@ -4,22 +4,8 @@ import org.springframework.security.core.context.SecurityContextHolder;
 
 public class TenantContext {
 
-    private static final ThreadLocal<String> overrideTenantId = new InheritableThreadLocal<>();
-
     public static String getTenantId() {
-        if (overrideTenantId.get() == null) {
-            final TenantAdherent principal = (TenantAdherent) SecurityContextHolder.getContext().getAuthentication().getPrincipal();
-            return principal.getTenantId();
-        } else {
-            return overrideTenantId.get();
-        }
-    }
-
-    public static void override(String tenantId) {
-        overrideTenantId.set(tenantId);
-    }
-
-    public static void reset() {
-        overrideTenantId.set(null);
+        final TenantAdherent principal = (TenantAdherent) SecurityContextHolder.getContext().getAuthentication().getPrincipal();
+        return principal.getTenantId();
     }
 }
